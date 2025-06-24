cad2a2c310c9fb47ac26a9d72c3183239341404e
Lahori Venkatesh <lahorivenkatesh709@gmail.com>
2025-06-24 22:28:18 +0530

    Updated HeroSection.tsx

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -196,7 +196,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({
   ];
   
   return (
-    <section className={`relative min-h-screen flex items-center justify-center pt-20 pb-20 px-4 sm:px-6 lg:px-8 overflow-hidden  ${
+    <section className={`relative min-h-screen flex items-center justify-center pt-20  px-4 sm:px-6 lg:px-8 overflow-hidden  ${
       isDarkMode ? 'text-white' : 'text-slate-900'
     }`}>
       <BackgroundPattern isDarkMode={isDarkMode} />
